0c52f94c898dde10a87e4ec5926b50b315f73fc0
Daria Sh <dsh.f.dev@gmail.com>
2025-12-28 18:21:06 +0300

    feat: создан компонент Layout

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -3,14 +3,12 @@ import React, { type PropsWithChildren } from 'react';
 import { Footer } from './components/footer';
 import { Header } from './components/header';
 
-export const Layout = ({children}: PropsWithChildren) => {
+export const Layout = ({ children }: PropsWithChildren) => {
   return (
     <>
-      <Header/>
-      <main>
-        {children}
-      </main>
-      <Footer/>
+      <Header />
+      <main>{children}</main>
+      <Footer />
     </>
   );
 };
