e309ae59932309f27faaeaf3e26f9091abcc61d6
Daria Sh <dsh.f.dev@gmail.com>
2025-12-29 20:57:15 +0300

    feat: добавлены стили для layout

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -3,12 +3,14 @@ import React, { type PropsWithChildren } from 'react';
 import { Footer } from './components/footer';
 import { Header } from './components/header';
 
+import styles from './layout.module.scss';
+
 export const Layout = ({ children }: PropsWithChildren) => {
   return (
-    <>
+    <div className={styles.layout}>
       <Header />
-      <main>{children}</main>
+      <main className={styles.main}>{children}</main>
       <Footer />
-    </>
+    </div>
   );
 };
