a8d69875a140fdd11632ff0a0fbadf7af2a88e0b
Daria Sh <dsh.f.dev@gmail.com>
2026-01-09 13:25:52 +0300

    refactor: отредактирован компонент PhoneCallButton

--- src/components/layout/components/phone-call-button/phone-call-button.tsx
+++ src/components/layout/components/phone-call-button/phone-call-button.tsx
@@ -1,8 +1,36 @@
+import { PhoneIcon } from '@/assets/icons';
+import { IconButton } from '@/components/icon-button';
+import { useWindowSize } from '@/hooks';
+
 import styles from './phone-call-button.module.scss';
 
-export const PhoneCallButton = () => {
+const TEL_NUMBER = '88000001122';
+
+interface PhoneCallButtonProps {
+  isFromFooter?: boolean;
+}
+
+export const PhoneCallButton = ({ isFromFooter = false }: PhoneCallButtonProps) => {
+  const { width } = useWindowSize();
+
+  if (isFromFooter) {
+    return (
+      <a href={`tel:${TEL_NUMBER}`} className={styles.phoneCall}>
+        +7 800 000 11 22
+      </a>
+    );
+  }
+
+  if (width <= 1024) {
+    return (
+      <IconButton href={`tel:${TEL_NUMBER}`}>
+        <PhoneIcon />
+      </IconButton>
+    );
+  }
+
   return (
-    <a href="tel:88000001122" className={styles.phoneCall}>
+    <a href={`tel:${TEL_NUMBER}`} className={styles.phoneCall}>
       +7 800 000 11 22
     </a>
   );
