4197026c722f5539bdf66ab1a6f28f8d2b4b6f13
Daria Sh <dsh.f.dev@gmail.com>
2026-01-18 23:41:58 +0300

    refactor: отредактирован компонент MainTeachers

--- src/modules/main-page-content/components/main-teachers/main-teachers.tsx
+++ src/modules/main-page-content/components/main-teachers/main-teachers.tsx
@@ -1,25 +1,21 @@
 import { useContext } from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
 
 import { Container } from '@/components/container';
 import { MainPageContext } from '@/store/main-page';
 
+import { TeachersSlider } from './components/teachers-slider';
+
 import styles from './main-teachers.module.scss';
 
 export const MainTeachers = () => {
-  const teachers = useContext(MainPageContext);
-
-  console.log(teachers);
+  const { teachersList } = useContext(MainPageContext);
 
   return (
     <section id="teachers" className={styles.teachers}>
       <Container>
         <div className={styles.wrapper}>
           <h2 className={styles.title}>Профессиональные тренеры</h2>
-          <Swiper>
-            <SwiperSlide></SwiperSlide>
-          </Swiper>
+          <TeachersSlider teachersList={teachersList} />
         </div>
       </Container>
     </section>
