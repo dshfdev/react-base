f03d7d909612929385262e60a29f0e7ba10bf204
Daria Sh <dsh.f.dev@gmail.com>
2026-01-24 18:04:01 +0300

    feat: реализован компонент Input

---
+++ src/components/input/input.tsx
@@ -0,0 +1,48 @@
+import { forwardRef, type InputHTMLAttributes } from 'react';
+
+import styles from './input.module.scss';
+
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  label?: string;
+  error?: string;
+  additionalClassname?: string;
+}
+
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ error, additionalClassname, ...rest }, refInput) => {
+    const inputId = rest.id || rest.name;
+    const createClassname = () => {
+      const baseClassname = `${styles.input}`;
+
+      if (additionalClassname) {
+        return `${baseClassname} ${additionalClassname}`;
+      }
+
+      if (error) {
+        return `${baseClassname} ${styles.error}`;
+      }
+
+      return baseClassname;
+    };
+
+    return (
+      <div className={styles.inputWrapper}>
+        {Boolean(rest.label) && (
+          <label className={styles.label} htmlFor={inputId}>
+            {rest.label}
+          </label>
+        )}
+        <input
+          id={inputId}
+          className={createClassname()}
+          ref={refInput}
+          {...rest}
+        />
+        {error && <span className={styles.errorText}>{error}</span>}
+      </div>
+    );
+  },
+);
+
+Input.displayName = 'Input';
