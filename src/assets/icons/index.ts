c98b17b102373cab5302eb40b419ee915c2a3aec
Daria Sh <dsh.f.dev@gmail.com>
2026-02-02 22:32:28 +0300

    fix(icons): отредактированы импорты иконок

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,6 +1,17 @@
+import { ArrowDownIcon } from './ArrowDownIcon';
+import { ArrowLeftIcon } from './ArrowLeftIcon';
+import { ArrowRightIcon } from './ArrowRightIcon';
 import { BurgerIcon } from './BurgerIcon';
 import { CloseIcon } from './CloseIcon';
 import { PhoneIcon } from './PhoneIcon';
 import { TooltipIcon } from './TooltipIcon';
 
-export { BurgerIcon, CloseIcon, PhoneIcon, TooltipIcon };
+export {
+  ArrowDownIcon,
+  ArrowLeftIcon,
+  ArrowRightIcon,
+  BurgerIcon,
+  CloseIcon,
+  PhoneIcon,
+  TooltipIcon,
+};
