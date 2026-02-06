eec970d8ec91604cf5eaabb6e7791b5232cb872a
Daria Sh <dsh.f.dev@gmail.com>
2026-02-06 13:43:20 +0300

    feat: создан компонент AboutPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import { Layout } from '@/components/layout';
+import { AboutPage } from '@/pages/about-page';
 import { MainPage } from '@/pages/main-page';
 
 import './styles/index.scss';
@@ -10,7 +11,7 @@ export const App = () => (
     <Routes>
       <Route element={<Layout />}>
         <Route path="/" element={<MainPage />} />
-        {/*<Route path="/about" element={<AboutPage />} />*/}
+        <Route path="/about" element={<AboutPage />} />
       </Route>
       {/*<Route path="/teacher/:teacherId" element={<TeacherModalPage />} />*/}
     </Routes>
